d611399b464b1261111b4dac56223a5521c78900
Robby Ciliberto <robby@ciliber.to>
2022-10-21 12:48:59 -0400

    A6 - Labs + Tuiter

---
+++ src/dev/previews.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
+import {PaletteTree} from './palette';
+import App from "../App";
+import Labs from "../labs";
+import HelloWorld from "../labs/a6/hello-world";
+import Assignment6 from "../labs/a6";
+import WhoToFollowListItem from "../tuiter/who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "../tuiter/who-to-follow-list";
+import PostSummaryItem from "../tuiter/post-summary-list/post-summary-item";
+import PostSummaryList from "../tuiter/post-summary-list";
+import ExploreComponent from "../tuiter/explore";
+
+const ComponentPreviews = () => {
+  return (
+    <Previews palette={<PaletteTree/>}>
+      <ComponentPreview path="/App">
+        <App/>
+      </ComponentPreview>
+      <ComponentPreview path="/Labs">
+        <Labs/>
+      </ComponentPreview>
+      <ComponentPreview path="/HelloWorld">
+        <HelloWorld/>
+      </ComponentPreview>
+      <ComponentPreview path="/Assignment6">
+        <Assignment6/>
+      </ComponentPreview>
+      <ComponentPreview path="/WhoToFollowListItem">
+        <WhoToFollowListItem/>
+      </ComponentPreview>
+      <ComponentPreview path="/WhoToFollowList">
+        <WhoToFollowList/>
+      </ComponentPreview>
+      <ComponentPreview path="/PostSummaryItem">
+        <PostSummaryItem/>
+      </ComponentPreview>
+      <ComponentPreview path="/PostSummaryList">
+        <PostSummaryList/>
+      </ComponentPreview>
+      <ComponentPreview path="/ExploreComponent">
+        <ExploreComponent/>
+      </ComponentPreview>
+    </Previews>
+  );
+};
+
+export default ComponentPreviews;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <DevSupport ComponentPreviews={ComponentPreviews}
+                useInitialHook={useInitial}
+    >
+      <App/>
+    </DevSupport>
   </React.StrictMode>
 );
 
